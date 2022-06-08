33ebe87d7ec9ef17c3f4bc987105bca2bee08b26
BJSchliep <sch16104@byui.edu>
2022-06-07 19:46:45 -0600

    commit

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is you first name");
+            Console.WriteLine("My name is Brandon");
+
+        }
+    }
+}
